e81d9b2d26c1892947f73f60e75efea32cc8a018
Alexander Wiltz <alwi1002@stud.hs-kl.de>
2024-10-10 07:37:33 +0200

    added start attribute to command line runner

--- src/main/java/de/hskl/itanalyst/alwi/PathfindingApplication.java
+++ src/main/java/de/hskl/itanalyst/alwi/PathfindingApplication.java
@@ -6,6 +6,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
@@ -30,7 +31,9 @@ public class PathfindingApplication extends SpringBootServletInitializer {
 
     // run just a single time if there is no data in database
     @Bean
+    @ConditionalOnProperty(name = "app.runner.enabled", havingValue = "true")
     public CommandLineRunner loadInitialData(FileHandlerService fileHandlerService) {
+        log.info("Command Line Runner is active");
         return (args) -> {
             String filename = "C:\\workspace\\osm-pathifinding\\src\\main\\resources\\osm\\export_ueberherrn+wohnstadt_nw.osm";
             fileHandlerService.saveFileDataToDatabase(filename);
