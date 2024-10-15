aa537e049840d19069560366d97b09a7eed155a9
Alexander Wiltz <alexander.wiltz@zf.com>
2024-10-15 14:09:24 +0200

    added toast message for error handling

--- src/main/resources/webapp/js/script.js
+++ src/main/resources/webapp/js/script.js
@@ -4,6 +4,6 @@ const toastLiveExample = document.getElementById('liveToast')
 if (toastTrigger) {
     const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
     toastTrigger.addEventListener('click', () => {
-        toastBootstrap.show()
+        toastBootstrap.show();
     })
 }
